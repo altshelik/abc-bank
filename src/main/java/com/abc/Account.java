138a8214be0fcc93bfc8cd8242386b3f527ff4c2
Elik Altshuler <altshelik@gmail.com>
2016-01-25 16:20:27 -0500

    One more bad smell in code.

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -11,7 +11,7 @@ public class Account {
     public static final int MAXI_SAVINGS = 2;
 
     private final int accountType;
-    public List<Transaction> transactions;
+    public List<Transaction> transactions; // TODO this breaks encapsulation. Variable should be private
 
     public Account(int accountType) {
         this.accountType = accountType;
